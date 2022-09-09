ee5fcab8e4395d7f75c3b7183c781fce8b812912
crookrtk <82241891+crookrtk@users.noreply.github.com>
2022-09-09 15:38:47 -0700

    Update RelentomHubSOURCE.lua

--- hub/launcher/source/RelentomHubSOURCE.lua
+++ hub/launcher/source/RelentomHubSOURCE.lua
@@ -1,10 +1,10 @@
 local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/crookrtk/RHUB/main/discordlib.txt")()
 
-local win = DiscordLib:Window("Relentom Hub v1.0b")
+local win = DiscordLib:Window("Relentom Hub v1.1b")
 
-local serv = win:Server("Scripts", "")
+local serv = win:Server("VR Scripts", "")
 
-local btns = serv:Channel("vr-section")
+local btns = serv:Channel("relentom-exclusives")
 
 btns:Button("RelentomVR V1", function(bool)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/crookrtk/RHUB/main/Relentom%20V1%20Medihood.lua"))()
@@ -14,7 +14,15 @@ end)
 
 btns:Seperator()
 
-btns:Button("MW VR", function(bool)
+btns:Button("CLOVR Hat Version (To be fixed)", function(bool)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/crookrtk/RHUB/main/hub/launcher/clovrhatver.lua"))()
+print(bool)
+DiscordLib:Notification("Notification", "Currently broken as of now, stay tuned for fixes!", "Okay!")
+end)
+
+local btns = serv:Channel("mw-vr")
+
+btns:Button("MW VR (SNIPER)", function(bool)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/crookrtk/RHUB/main/MW_VR.lua"))()
 print(bool)   
 DiscordLib:Notification("Notification", "Launched, credit to MyWorld#4430", "Okay!")
@@ -22,79 +30,18 @@ end)
 
 btns:Seperator()
 
-btns:Button("MW VR DA HOOD", function(bool)
+btns:Button("MW VR DA HOOD (SNIPER)", function(bool)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/crookrtk/RHUB/main/MW_VR%20(2).txt"))()
 print(bool)   
 DiscordLib:Notification("Notification", "Launched, credit to MyWorld#4430 , patches made to work in Da Hood", "Okay!")
 end)
 
-btns:Seperator()
+local btns = serv:Channel("fake-vr")
 
-btns:Button("CLOVR Hat Version (Broken?)", function(bool)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/crookrtk/RHUB/main/hub/launcher/clovrhatver.lua"))()
-print(bool)
-DiscordLib:Notification("Notification", "Launched, be sure you are wearing the right hats!", "Okay!")
-end)
-
-btns:Seperator()
-
-btns:Button("AnarksVR", function(bool)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/crookrtk/anarksVR/main/forloadstring.lua"))()
-print(bool)
-DiscordLib:Notification("Notification", "Launched, FULL Credit to saucekid, as this is just my fixed version of the script!", "Okay!")
-end)
-
-local tgls = serv:Channel("coming-soon")
-
-tgls:Toggle("Coming Soon",false, function(bool)
-print(bool)
-end)
-
-local sldrs = serv:Channel("coming-soon")
-
-local sldr = sldrs:Slider("Coming Soon", 0, 1000, 400, function(t)
-print(t)
-end)
-
-sldrs:Button("Change to 50", function()
-sldr:Change(50)
-end)
-
-local drops = serv:Channel("coming-soon")
-
-
-local drop = drops:Dropdown("Coming Soon",{"Option 1","Option 2","Option 3","Option 4","Option 5"}, function(bool)
-print(bool)
-end)
-
-drops:Button("Clear", function()
-drop:Clear()
-end)
-
-drops:Button("Add option", function()
-drop:Add("Option")
-end)
-
-local clrs = serv:Channel("coming-soon")
-
-clrs:Colorpicker("ESP Color", Color3.fromRGB(255,1,1), function(t)
-print(t)
-end)
-
-local textbs = serv:Channel("coming-soon")
-
-textbs:Textbox("Coming Soon", "Type here!", true, function(t)
-print(t)
-end)
-
-local lbls = serv:Channel("coming-soon")
-
-lbls:Label("This is just a label.")
-
-local bnds = serv:Channel("coming-soon")
-
-bnds:Bind("Coming Soon", Enum.KeyCode.RightShift, function()
-print("Killed everyone!")
+btns:Button("Fake Sked's VR", function(bool)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/crookrtk/RHUB/main/hub/launcher/fakesked.lua"))()
+print(bool)   
+DiscordLib:Notification("Notification", "Launched!", "Okay!")
 end)
 
 local serv = win:Server("Credits", "")
